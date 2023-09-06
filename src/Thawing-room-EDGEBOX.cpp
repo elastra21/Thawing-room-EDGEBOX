8762db325f048ec535043e0f28a1ba28dcc7e639
hugobienvenidos <hugo.bienvenidos@gmail.com>
2023-09-05 20:03:22 -0700

    Ts Tc in float + coef at Ts because different R

--- src/Thawing-room-EDGEBOX.cpp
+++ src/Thawing-room-EDGEBOX.cpp
@@ -947,10 +947,13 @@ void updateTemperature() {
   float TA_analog = analog_inputs.readADC_SingleEnded(TA_AI);  // Ta
   // TA = (TA_analog - 120) * (320 + 30) / (32768 - 120) - 30;
   TA = (TA_analog - 2708) * (50 + 20) / (13284 - 2708) - 20;
-  uint16_t TS_analog = analog_inputs.readADC_SingleEnded(TS_AI);  // Ts
-  TS = (TS_analog - 2708) * (50 + 20) / (13284 - 2708) - 20;
-  uint16_t TC_analog = analog_inputs.readADC_SingleEnded(TC_AI);  // Tc
+
+  float TS_analog = analog_inputs.readADC_SingleEnded(TS_AI);  // Ts
+  TS = ((TS_analog - 2708) * (50 + 20) / (13284 - 2708) - 20)*1.8;
+
+  float TC_analog = analog_inputs.readADC_SingleEnded(TC_AI);  // Tc
   TC = (TC_analog - 2708) * (50 + 20) / (13284 - 2708) - 20;
+
   TI = sensors1.getTempC(ADDRESS_TI);  // Ti
 
   // TC1 = sensors1.getTempC(ADDRESS_TC1);  //PV /Ta
