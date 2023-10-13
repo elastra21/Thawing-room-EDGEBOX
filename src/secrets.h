479298844f65a508e4e2a57b4ba2ff58db253006
hugobienvenidos <hugo.bienvenidos@gmail.com>
2023-10-13 03:11:06 -0700

    change wifi and hostname auto

--- src/secrets.h
+++ src/secrets.h
@@ -1,8 +1,8 @@
 // #define SECRET_SSID "mfpguest_2.4Ghz@unifi"  // replace MySSID with your WiFi network name
 // #define SECRET_SSID "mfpguest_2.4Ghz
 // #define SECRET_SSID "GFguestwifi_AP"
-#define SECRET_SSID "MFPGuestWifi_FTY1"  // replace MySSID with your WiFi network name
-#define SECRET_PASS "Pr0@#17774$$M@f1"
+// #define SECRET_SSID "MFPGuestWifi_FTY1"  // replace MySSID with your WiFi network name
+// #define SECRET_PASS "Pr0@#17774$$M@f1"
 // #define SECRET_SSID "GFguestwifi_2.4ghz@unifi" 
 // #define SECRET_PASS "GF@9300@a"
 // #define SECRET_SSID "Pez Gordo" 
@@ -11,6 +11,8 @@
 // #define SECRET_PASS "35588590"
 // #define SECRET_SSID "TP-Link_EE55"
 // #define SECRET_PASS "42047483"
+#define SECRET_SSID "MFP-Guest24" 
+#define SECRET_PASS "testing123"
 #define HOST_NAME "esp32"
 // #define IP_ADDRESS "192.168.1.160" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
 // #define IP_ADDRESS "172.16.30.140" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
