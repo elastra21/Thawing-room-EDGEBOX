0a0fbaa4b343788df496a1eaffe86f7accaff697
hugobienvenidos <hugo.bienvenidos@gmail.com>
2023-12-04 11:00:07 -0800

    IP changed

--- src/secrets.h
+++ src/secrets.h
@@ -3,21 +3,21 @@
 // #define SECRET_SSID "GFguestwifi_AP"
 // #define SECRET_SSID "MFPGuestWifi_FTY1"  // replace MySSID with your WiFi network name
 // #define SECRET_PASS "Pr0@#17774$$M@f1"
-#define SECRET_SSID "GFguestwifi_2.4ghz@unifi" 
-#define SECRET_PASS "GF@9300@a"
+// #define SECRET_SSID "GFguestwifi_2.4ghz@unifi" 
+// #define SECRET_PASS "GF@9300@a"
 // #define SECRET_SSID "Pez Gordo" 
 // #define SECRET_PASS "SardinaMacarena2021"
 // #define SECRET_SSID "Maxis_9FF4D2"
 // #define SECRET_PASS "35588590"
-// #define SECRET_SSID "MFP-Guest24" 
-// #define SECRET_PASS "testing123"
+#define SECRET_SSID "MFP-Guest24" 
+#define SECRET_PASS "testing123"
 // #define SECRET_SSID "GFguestwifi_AP"
 // #define SECRET_PASS "GF@9300@a" 
 #define HOST_NAME "esp32"
 // #define IP_ADDRESS "192.168.1.160" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
 // #define IP_ADDRESS "172.16.30.140" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
 // #define IP_ADDRESS "192.168.0.196" //previous ip : 10,0,3,21, replace that ip to match the IP of the device where the broker is running
-#define IP_ADDRESS "192.168.1.152" // MFP-Guest24 on MFP V1
+#define IP_ADDRESS "192.168.1.202" // MFP-Guest24 on MFP V1
 // #define IP_ADDRESS "192.168.1.151" // GFGuestwifi_AP on GF Big
 
 #define PORT      1883
